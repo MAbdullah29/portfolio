83cf035dc750c9e1864cd0ed92b4825018c05b3d
Muhammad Usman <muhmmmadusman0456@gmail.com>
2025-11-13 22:55:54 +0500

    site structure

--- src/Pages/layout/Layout.jsx
+++ src/Pages/layout/Layout.jsx
@@ -6,15 +6,12 @@ import Header from "../../Shared/Header";
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen ">
-      {/* Header - Always visible */}
       <Header />
 
-      {/* Main Content */}
       <main className=" m-0 p-0">
         {children}
       </main>
 
-      {/* Footer - Always visible */}
       <Footer />
     </div>
   );
